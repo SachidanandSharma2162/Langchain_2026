f9119d514944bef9a3b44ddd0230ed5d23e15c01
SachidanandSharma2162 <sharmasachidanand1111@gmail.com>
2026-04-25 14:13:39 +0530

    Chat Model Done.

---
+++ ChatModels/huggingface_local_model.py
@@ -0,0 +1,16 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint, HuggingFacePipeline
+
+llm = HuggingFacePipeline(
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    pipeline_kwargs=dict(
+        max_new_tokens=100,
+        temperature=0.7,
+    )
+)
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of India")
+
+print(result.content)
