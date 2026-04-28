471ef4be32e9160972f48aa9888199779d48af07
SachidanandSharma2162 <sharmasachidanand1111@gmail.com>
2026-04-28 13:43:25 +0530

    chatPromptTemplate and meaasgePlaceholder.

---
+++ Prompts/messagePlaceholder.py
@@ -0,0 +1,18 @@
+from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
+
+chat_template=ChatPromptTemplate.from_messages([
+    ("system","You are a helpful agent."),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("human","{input}")
+])
+
+chat_history=[]
+
+with open("chat_history.txt") as f:
+    chat_history.extend(f.readlines())
+
+print(chat_history)
+
+prompt = chat_template.invoke({'chat_history':chat_history, 'query':'Where is my refund'})
+
+print(prompt)
